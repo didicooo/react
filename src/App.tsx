04ec28cc3b594c2c10c88d1b12c2d640d25c4885
Adriano <adriano.filho@aluno.lsb.com.br>
2025-09-16 20:59:34 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      site do didico
+
+      <p>minimalismo
+        <p>apenas o necessario
+    
+        </p>
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div> id="rodape""
+<p>adriano adriano adriano adriano adriano adriano adriano adriano adriano adriano adriano
+    <p>
+        </p></p></div>
+    )
+}
+
+export default Footer;
